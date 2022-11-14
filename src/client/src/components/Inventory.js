cbf509f099da2120d781e12953ddfd76f0337ad7
harunm1 <harun.mohammad2109@gmail.com>
2022-11-14 17:57:33 -0500

    Added pics of items

--- src/client/src/components/Inventory.js
+++ src/client/src/components/Inventory.js
@@ -14,26 +14,26 @@ function Inventory() {
             {
             isInventoryOpen ? (
                 <div className='Grid'>
-                    <div className='r1'>
-                        <div className = 'c1'>
-                            1
+                    <div className='row'>
+                        <div className = 'item'>
+                            <img src={require('../assets/mushroom.png')} alt="mushroom"/>
                         </div>
-                        <div className = 'c2'>
-                            2
+                        <div className = 'item'>
+                            <img src={require('../assets/wheat.png')} alt="wheat"/>
                         </div>
-                        <div className = 'c3'>
-                            3
+                        <div className = 'item'>
+                            <img src={require('../assets/potato.png')} alt="potato"/>
                         </div>
                     </div>
-                    <div className='r2'>
-                        <div className = 'c4'>
-                            4
+                    <div className='row'>
+                        <div className = 'item'>
+                            <img src={require('../assets/bamboo.png')} alt="bamboo"/>
                         </div>
-                        <div className = 'c5'>
-                            5
+                        <div className = 'item'>
+                            <img src={require('../assets/lavendar.png')} alt="lavendar"/>
                         </div>
-                        <div className = 'c6'>
-                            6
+                        <div className = 'item'>
+                            <img src={require('../assets/corn.png')} alt="corn"/>
                         </div>
                     </div>
 
