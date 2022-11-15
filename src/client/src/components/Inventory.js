a2ff93b71380393d3319f237e8a7ba17d589fb73
harunm1 <harun.mohammad2109@gmail.com>
2022-11-14 20:42:16 -0500

    Added boxes for quantities of items

--- src/client/src/components/Inventory.js
+++ src/client/src/components/Inventory.js
@@ -17,27 +17,51 @@ function Inventory() {
                     <div className='row'>
                         <div className = 'item'>
                             <img src={require('../assets/mushroom.png')} alt="mushroom"/>
+                            <div className='item-info'>
+                                <h4>Quantity:</h4>
+                                <p id='mushroomQuantity'>0</p>    
+                            </div>
                         </div>
                         <div className = 'item'>
                             <img src={require('../assets/wheat.png')} alt="wheat"/>
+                            <div className='item-info'>
+                                <h4>Quantity:</h4>
+                                <p id='mushroomQuantity'>0</p>    
+                            </div>
                         </div>
                         <div className = 'item'>
                             <img src={require('../assets/potato.png')} alt="potato"/>
+                            <div className='item-info'>
+                                <h4>Quantity:</h4>
+                                <p id='mushroomQuantity'>0</p>    
+                            </div>
                         </div>
                     </div>
                     <div className='row'>
                         <div className = 'item'>
                             <img src={require('../assets/bamboo.png')} alt="bamboo"/>
+                            <div className='item-info'>
+                                <h4>Quantity:</h4>
+                                <p id='mushroomQuantity'>0</p>    
+                            </div>
                         </div>
                         <div className = 'item'>
                             <img src={require('../assets/lavendar.png')} alt="lavendar"/>
+                            <div className='item-info'>
+                                <h4>Quantity:</h4>
+                                <p id='mushroomQuantity'>0</p>    
+                            </div>
                         </div>
                         <div className = 'item'>
                             <img src={require('../assets/corn.png')} alt="corn"/>
+                            <div className='item-info'>
+                                <h4>Quantity:</h4>
+                                <p id='mushroomQuantity'>0</p>    
+                            </div>
                         </div>
                     </div>
 
-                <button onClick={()=> onClick()}>Close</button> 
+                <button className='close-inventory-button' onClick={()=> onClick()}>Close</button> 
                 </div>
             )
             :  
