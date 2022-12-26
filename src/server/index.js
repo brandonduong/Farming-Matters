457aedf1c44ef324438ce14b96d01e3f8cbe0a80
mihail-serafim <mihail.s.mk@gmail.com>
2022-12-26 16:40:09 -0500

    added logging back

--- src/server/index.js
+++ src/server/index.js
@@ -26,28 +26,38 @@ app.post('/', (req, res) => {
 });
 
 io.on('connection', async (socket) => {
+  console.log(`\na user connected, id ${socket.id}`);
+  
   const userID = '123'; // client should send userID once user is authenticated. See https://socket.io/docs/v4/middlewares/#sending-credentials
   let isSessionActive;
 
   // Query the database for the active session ID corresponding to the current user
   await redis.get(userID, (err, sessionID) => {
-    if (err) console.error(err);
-    else {
+    if (err) {
+      console.error(err);
+    } else {
       isSessionActive = socket.id === sessionID || !sessionID;
+
+      console.log(`current: ${socket.id}`);
+      console.log(`active: ${sessionID}`);
     }
   });
   
-  if (isSessionActive) {
-    redis.set(userID, socket.id)
-  } 
-  else {
+  if (!isSessionActive) {
+    console.log(`denied!!! socket: ${socket.id}`)
     socket.emit('deny')
+  } else {
+    // if session is active, set as active in db
+    const expirySeconds = 60*60*12 // 60 secs * 60 mins * 12 hrs = 1/2 day
+    redis.set(userID, socket.id, "NX", "EX", expirySeconds)
   }
 
-  socket.on('disconnect', () => {
+  socket.on('disconnecting', () => {
     if (isSessionActive){
+      console.log("active user disconnected, deleting...")
       redis.del(userID)
     }
+    console.log(`user disconnected, id ${socket.id}`);
   })
 });
 
