553e1628418756b989df4ae5df1d1565e8e77385
Namit Chopra <namitc.11@gmail.com>
2023-01-10 13:49:22 -0500

    added a logging method

--- src/server/index.js
+++ src/server/index.js
@@ -1,13 +1,51 @@
 const express = require("express");
+const mysql = require("mysql");
+const app = express();
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const PORT = process.env.PORT || 5001;
 
-const app = express();
-app.use(express.json())
+// For allowing access to API from other server than host
+app.use(cors());
 
-app.post('/', (req, res) => {
-    console.log(req.body);
-    res.status(200).send();
+app.use(express.json());
+
+// maybe?
+// app.use(express.urlencoded({ extended: false }));
+
+// connection details
+let db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: process.env.PASSWORD,
+  database: "testFarmingMatters",
+});
+
+// connect to database
+db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log("MySql connected...");
+});
+
+app.post("/", (req, res) => {
+  let action = `Action: ${req.body["action"]}`;
+  console.log(JSON.stringify(req.body));
+  let action_entry = {
+    user_id: 1,
+    action: JSON.stringify(req.body),
+    time: new Date().toLocaleString(),
+  };
+  let sql = "INSERT INTO LoggedActions SET ?";
+  let query = db.query(sql, action_entry, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+  });
+  console.log(req.body);
+  res.status(200).send();
 });
 
 app.listen(PORT, () => {
