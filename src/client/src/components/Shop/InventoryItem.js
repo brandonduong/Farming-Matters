8e46fe350d62d6e62c52f945ca168733365872f5
harunm1 <harun.mohammad2109@gmail.com>
2023-02-04 18:30:30 -0500

    WIP: fixing the price changes for every turn

--- src/client/src/components/Shop/InventoryItem.js
+++ src/client/src/components/Shop/InventoryItem.js
@@ -1,18 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {globalInventoryContext} from "../../App";
 import { addItem, getItemCount, getItems, removeItem } from "../Inventory";
 
 const InventoryItem = (props) => {
   const [quantity, setQuantity] = useState(0);
   const { inventoryState, insuredState } = React.useContext(globalInventoryContext);
-  let hasSeasonChanged = props.turn % 3 == 1;
+  // let bestPrice = props.price;
+  const [ bestPrice, setBestPrice] = useState(props.price);
+  // let hasTurnChanged = props.turn % 2 == 1;
 
   function sell() {
       if(parseInt(getItemCount(inventoryState,props.name)) < quantity || parseInt(getItemCount(inventoryState,props.name)) === 0){
         console.log("Not enough items to sell")
       }
       else{
-        props.setMoney(props.money + quantity * props.price);
+        props.setMoney(props.money + quantity * bestPrice);
         setQuantity(0);
         removeItem(inventoryState,props.name,quantity);
         console.log(inventoryState);
@@ -29,20 +31,36 @@ const InventoryItem = (props) => {
   }
 
   function chooseBestPrice() {
-      let currBestPrice = fluctuatePrice(props.price);
-      if (getItemCount(insuredState,props.name) > 0){
-        if (props.price > currBestPrice){
-          currBestPrice = props.price;
+    // start by seeing if prices fluctuate every turn
+      let previous = props.price;
+      let fluctuate = fluctuatePrice(props.price);
+      let hasEnoughInsuredItems = getItemCount(insuredState,props.name) > 0;
+      // if the person has insurance they will sell the item for atleast the same price it was bought for
+      if (hasEnoughInsuredItems){
+        if (previous > fluctuate){
+          setBestPrice(previous);
+        }
+        else{
+          setBestPrice(fluctuate)
         }
       }
-      return currBestPrice;
+      // if the person has not ensured the item then it will fluctuate the next turn 
+      else {
+        setBestPrice(fluctuate);
+      }
   }
+
+  useEffect(() => {
+    chooseBestPrice()
+    console.log(bestPrice);
+  }, [props.turn])
+
 
   return (
     <div className="shop-item" key={props.id }>
       <img src={props.image} alt="crops" className="item-image"></img>
       <p style={{ color: "white", margin: "5px" }}>
-        {props.name + " - $" + chooseBestPrice()}
+        {props.name + " - $" + bestPrice}
       </p>
       <label htmlFor="quantity" style={{ color: "white" }}>
         Quantity:
