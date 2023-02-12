7572a1bc4fc81a556a5c94e0b0d85b1f8c633817
harunm1 <harun.mohammad2109@gmail.com>
2023-02-11 19:33:25 -0500

    Adding proper pics for extra items

--- src/client/src/components/Shop/constants.js
+++ src/client/src/components/Shop/constants.js
@@ -10,85 +10,85 @@ export const shopItemsList = [
       id: 1,
       name: "Rice",
       price: 100,
-      // from https://www.flaticon.com/free-icons/corn
-      image: require("../../assets/CropIcons/corn.png"),
+      // from https://www.flaticon.com/free-icon/rice
+      image: require("../../assets/CropIcons/rice.png"),
     },
     {
       id: 2,
       name: "Carrot",
       price: 200,
-      // from https://www.freepik.com/free-vector/vegetable-icons-collection_948406.htm#query=corn%20icon&position=2&from_view=search&track=sph
-      image: require("../../assets/CropIcons/tomato.png"),
+      // from https://www.flaticon.com/free-icon/carrot
+      image: require("../../assets/CropIcons/carrot.png"),
     },
     {
       id: 3,
       name: "Orange",
       price: 150,
-      // from https://www.freepik.com/free-vector/vegetable-icons-collection_948406.htm#query=corn%20icon&position=2&from_view=search&track=sph
-      image: require("../../assets/CropIcons/eggplant.png"),
+      // from https://www.flaticon.com/free-icon/orange
+      image: require("../../assets/CropIcons/orange.png"),
     },
     {
       id: 4,
       name: "Lettuce",
       price: 150,
-      // from https://www.freepik.com/free-vector/vegetable-icons-collection_948406.htm#query=corn%20icon&position=2&from_view=search&track=sph
-      image: require("../../assets/CropIcons/cucumber.png"),
+      // from https://www.flaticon.com/free-icon/lettuce
+      image: require("../../assets/CropIcons/lettuce.png"),
     },
     {
       id: 5,
       name: "Tomato",
       price: 125,
-      // from https://www.freepik.com/premium-vector/orange-pumpkin-vector-illustration-fall-season-symbol-thick-black-outline-single-object-hello-autumn_30810460.htm#query=pumpkin%20icon&position=16&from_view=search&track=sph
-      image: require("../../assets/CropIcons/pumpkin.png"),
+      // from https://www.flaticon.com/free-icon/tomato
+      image: require("../../assets/CropIcons/tomato.png"),
     },
     {
       id: 6,
       name: "Watermelon",
       price: 175,
-      // from https://www.freepik.com/free-vector/vegetable-icons-collection_948406.htm#query=corn%20icon&position=2&from_view=search&track=sph
-      image: require("../../assets/CropIcons/carrot.png"),
+      // from https://www.flaticon.com/free-icon/watermelon
+      image: require("../../assets/CropIcons/watermelon.png"),
     },
     {
       id: 7,
       name: "Wheat",
       price: 175,
-      // from https://www.freepik.com/free-vector/isolated-watermelon-cartoon-style_22725431.htm#query=watermelon%20icon&position=3&from_view=search&track=sph
-      image: require("../../assets/CropIcons/wintermelon.png"),
+      // from https://www.flaticon.com/free-icon/wheat
+      image: require("../../assets/CropIcons/wheat.png"),
     },
     {
       id: 8,
       name: "Pumpkin",
       price: 175,
-      // from https://www.flaticon.com/free-icons/potato
-      image: require("../../assets/CropIcons/wheat.png"),
+      // from https://www.flaticon.com/free-icons/pumpkin
+      image: require("../../assets/CropIcons/pumpkin.png"),
     },
     {
       id: 9,
       name: "Beet",
       price: 175,
-      // from https://www.flaticon.com/free-icons/potato
-      image: require("../../assets/CropIcons/wheat.png"),
+      // from https://www.flaticon.com/free-icons/beet
+      image: require("../../assets/CropIcons/beet.png"),
     },
     // Winter
     {
       id: 10,
       name: "Berries",
       price: 175,
-      // from https://www.flaticon.com/free-icons/potato
-      image: require("../../assets/CropIcons/wheat.png"),
+      // from https://www.flaticon.com/free-icons/berries
+      image: require("../../assets/CropIcons/berries.png"),
     },
     {
       id: 11,
       name: "Mushroom",
       price: 175,
-      // from https://www.flaticon.com/free-icons/potato
-      image: require("../../assets/CropIcons/wheat.png"),
+      // from https://www.flaticon.com/free-icons/mushroom
+      image: require("../../assets/CropIcons/mushrooms.png"),
     },
     {
       id: 12,
       name: "Wintermelon",
       price: 175,
-      // from https://www.flaticon.com/free-icons/potato
-      image: require("../../assets/CropIcons/wheat.png"),
+      // modified the watermelon pic
+      image: require("../../assets/CropIcons/wintermelon.png"),
     },
   ];
