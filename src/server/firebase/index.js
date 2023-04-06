8335a0445e4e8916aec8d71645496c51b2951b06
mihail-serafim <mihail.s.mk@gmail.com>
2023-04-05 20:39:04 -0400

    cleaning up backend

--- src/server/firebase/index.js
+++ src/server/firebase/index.js
@@ -1,3 +1,9 @@
+/**
+ * Initializes Firebase using the 'serviceAccount' secret and exports the auth instance
+ * used to interact with Firebase auth.
+ * See https://firebase.google.com/docs/auth/admin
+ */
+
 var admin = require("firebase-admin");
 const { initializeApp } = require('firebase-admin/app');
 const { getAuth } = require("firebase-admin/auth");
